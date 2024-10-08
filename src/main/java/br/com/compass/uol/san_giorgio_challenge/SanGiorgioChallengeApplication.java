736c300c5f8f37d118a50f777e96053d9650fbac
Jhonatas <jhonatasa_s@hotmail.com>
2024-10-07 22:15:06 -0300

    first commit

---
+++ src/main/java/br/com/compass/uol/san_giorgio_challenge/SanGiorgioChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.uol.san_giorgio_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanGiorgioChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanGiorgioChallengeApplication.class, args);
+	}
+
+}
